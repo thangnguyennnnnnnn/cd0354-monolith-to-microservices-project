82717a096f527ecdce09496fc6792283f4d4c2c9
Thắng <nguyenhuynhthang24@gmail.com>
2024-11-28 14:09:45 +0700

    Commit code

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=thangnh35
-export POSTGRES_PASSWORD='thangnh35'
+export POSTGRES_PASSWORD=thangnh35
 export POSTGRES_HOST=udagram.c5ybyvuoe7ki.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=udagram
 export AWS_BUCKET=udagram-s3
--- udagram-api-user/src/config/config.ts
+++ udagram-api-user/src/config/config.ts
@@ -1,14 +1,28 @@
+// export const config = {
+//   'username': process.env.POSTGRES_USERNAME,
+//   'password': process.env.POSTGRES_PASSWORD,
+//   'database': process.env.POSTGRES_DB,
+//   'host': process.env.POSTGRES_HOST,
+//   'dialect': 'postgres',
+//   'aws_region': process.env.AWS_REGION,
+//   'aws_profile': process.env.AWS_PROFILE,
+//   'aws_media_bucket': process.env.AWS_BUCKET,
+//   'url': process.env.URL,
+//   'jwt': {
+//     'secret': process.env.JWT_SECRET,
+//   },
+// };
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
+  'username': 'thangnh35',
+  'password': 'thangnh35',
+  'database': 'udagram',
+  'host': 'udagram.c5ybyvuoe7ki.us-east-1.rds.amazonaws.com',
   'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'udagram-s3',
+  'url': 'http://localhost:8100',
   'jwt': {
-    'secret': process.env.JWT_SECRET,
+    'secret': 'testing',
   },
 };
