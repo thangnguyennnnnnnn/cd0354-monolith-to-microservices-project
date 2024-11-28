0e9220a040a106bd9eaceced27e97f7bb7291560
Thắng <nguyenhuynhthang24@gmail.com>
2024-11-28 19:01:30 +0700

    1.6

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  'username': 'thangnh35',
-  'password': 'thangnh35',
-  'database': 'udagram',
-  'host': 'udagram.c5ybyvuoe7ki.us-east-1.rds.amazonaws.com',
+  'username': process.env.POSTGRES_USERNAME,
+  'password': process.env.POSTGRES_PASSWORD,
+  'database': process.env.POSTGRES_DB,
+  'host': process.env.POSTGRES_HOST,
   'dialect': 'postgres',
-  'aws_region': 'us-east-1',
-  'aws_profile': 'default',
-  'aws_media_bucket': 'udagram-s3',
-  'url': 'http://localhost:8100',
+  'aws_region': process.env.AWS_REGION,
+  'aws_profile': process.env.AWS_PROFILE,
+  'aws_media_bucket': process.env.AWS_BUCKET,
+  'url': process.env.URL,
   'jwt': {
-    'secret': 'testing',
+    'secret': process.env.JWT_SECRET,
   },
 };
