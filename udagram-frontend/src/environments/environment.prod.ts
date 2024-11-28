a67acea2d4afb3dda0c75fef201a1383560d2bcc
Thắng <nguyenhuynhthang24@gmail.com>
2024-11-28 16:19:01 +0700

    update config

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a1395ded9fc3a40ab92f909c7b82538d-1439867767.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
