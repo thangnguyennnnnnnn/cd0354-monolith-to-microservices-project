48019ea51da83ec291b0cddc713f0f4e9d5a8874
Thắng <nguyenhuynhthang24@gmail.com>
2024-11-28 08:36:17 +0700

    Commit code

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_USERNAME=thangnh35
+export POSTGRES_PASSWORD='thangnh35'
+export POSTGRES_HOST=udagram.c5ybyvuoe7ki.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagram
+export AWS_BUCKET=udagram-s3
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
